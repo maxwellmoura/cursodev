65ac1145cbbcdda0b627e0c21e19e989b63ff928
Maxwell Moura <maxwellcmoura@gmail.com>
2023-12-15 20:08:18 -0300

    Add files via upload
    Projeto de uma Ótica, com JavaScritp, CSS e React

---
+++ componentes/Rodape/Rodape.jsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+    <footer>
+        ₢ 2023 Ótica Vida - Todos os direitos reservados. Maxwell Moura        
+    </footer>
+    )
+}
---
+++ componentes/TopoConteudo/Banner.jsx
@@ -0,0 +1,13 @@
+export default function Banner(){
+    return (
+        <section>
+            <div className="banner banner-flex">
+                <div className="texto-banner">
+                    <h2>Preços baixos em</h2>
+                    <h1>óculos de <br></br>grau e de sol</h1>
+                    <h2>Você só encontra aqui</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
