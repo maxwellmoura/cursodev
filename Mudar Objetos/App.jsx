a8d81aba81af8ba719a2ca20d6311efcc0ec419a
Maxwell Moura <maxwellcmoura@gmail.com>
2024-01-08 16:08:54 -0300

    Update App.jsx

--- Mudar Objetos/App.jsx
+++ Mudar Objetos/App.jsx
@@ -3,21 +3,24 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [novoEstilo, setNovoEstilo] = useState("bg-vermelho");
+  const [novaCor, setNovaCor] = useState("bg-vermelho");
 
   const alterarCor = () => {
-    setNovoEstilo("bg-azul");
+    if (novaCor === "bg-vermelho") {
+      setNovaCor('bg-azul');
+    } else {
+      setNovaCor('bg-vermelho')
+    }
   };
 
   return (
     <div className="display">
       <div className="container">
         <h1>Alterar cor de fundo:</h1>
-        <div className={novoEstilo}></div>
+        <div className={novaCor}></div>
         <p>Clique no botão abaixo para alterar a cor da caixa.</p>
         <button onClick={alterarCor}>Mudar Cor</button>
       </div>
     </div>
   );
 }
-
