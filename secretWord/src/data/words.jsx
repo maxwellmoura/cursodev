002e0931c52bcc092544496ef3f7064429ecc6fc
Maxwell Moura <maxwellcmoura@gmail.com>
2024-03-27 10:18:09 -0300

    Add files via upload
    Projeto, de jogo de adivinhação. Curso da Udemy de React.

---
+++ secretWord/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList ={
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Vidro", 'Chave'],
+    fruta: ["Banana", "Maçã", "Uva", 'Mamão', 'Laranja', 'Abacaxi'],
+    corpo: ['Braço', 'Perna', 'Olhos', 'Pescoço', 'Lingua', 'Dentes'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Fonte', 'Processador'],
+    programação: ['React', 'JavaScript', 'Framework', 'html', 'Node', 'css'],
+    alimento: ['Arroz', 'Feijão', 'Macarrão', 'Carne', 'Leite', 'Ovo', 'Frango']
+};
